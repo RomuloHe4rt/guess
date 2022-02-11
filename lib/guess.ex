d3f2a0b53c44cd4620b9bf35cc96b39dab1a2259
Rômulo Silva <romulo_lss@hotmail.com>
2022-02-10 22:04:57 -0300

    handling data entry with pattern matching

--- lib/guess.ex
+++ lib/guess.ex
@@ -10,6 +10,15 @@ defmodule Guess do
     IO.puts("Let's play Guess the number")
 
     IO.gets("Pick a difficult level (1, 2 or 3): ")
+    |> Integer.parse()
+    |> parse_input()
     |> IO.inspect()
   end
+
+  def parse_input(:error) do
+    IO.puts("Invalid level!")
+    run()
+  end
+
+  def parse_input({num, _}), do: num
 end
